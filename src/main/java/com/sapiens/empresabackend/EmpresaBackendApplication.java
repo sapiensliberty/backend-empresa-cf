c09a9050d4bca25fa89424c6e19c3e2df18dd5cf
Victor Aguilar <elqtodolov@gmail.com>
2026-08-10 21:39:23 -0500

    Creando Backend con Spring Boot

---
+++ src/main/java/com/sapiens/empresabackend/EmpresaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sapiens.empresabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapiens/empresabackend/EmpresaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapiens.empresabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
